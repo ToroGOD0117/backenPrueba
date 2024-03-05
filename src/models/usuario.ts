aee260624467256289dc690f10b83ffb6c1078fd
ToroGOD0117 <tucorreo@example.com>
2024-03-05 11:54:09 -0500

    creacion del model usuario

---
+++ src/models/usuario.ts
@@ -0,0 +1,51 @@
+import { Model, Schema, model } from "mongoose";
+
+const UsuarioSchema = new Schema({
+    nombre:{
+        type:String,
+        require: true
+    },
+
+    tipoDocumento:{
+        type: String,
+        require: true,
+    },
+    numeroDocumento: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    login: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    pass: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    rol:{
+        type: Boolean,
+        require: true,
+    },
+    email:{
+        type: String,
+        require:true,
+        unique:true
+    },
+    estado:{
+        type: Boolean,
+        require:true,
+        default: true
+    },
+    createdAt:{
+        type:Date,
+        default: Date.now()
+    }
+
+});
+
+const UsuarioModel: Model <any> = model ("usuario", UsuarioSchema);
+
+export default UsuarioModel;
